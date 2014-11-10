45b972b75339af34a420ef581db8fac9910b1ed5
Xinyu <xywang@thoughtworks.com>
2014-11-10 18:08:23 +0530

    s

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,6 +16,6 @@ scp ./dist.tar root@112.124.43.62:/tmp/
 
 ssh root@112.124.43.62 /bin/bash << EOF
 rm -fr /opt/works/shumai_homepage/
-mkdir /opt/works/shumai_homepage/
+mkdir -p /opt/works/shumai_homepage/
 tar xvf /tmp/dist.tar -C /opt/works/shumai_homepage/
 EOF
