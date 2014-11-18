606ccf5f17de4ba66632b88157ab851f1511d666
Xinyu <xywang@thoughtworks.com>
2014-11-18 11:41:42 +0530

    snap ci script

---
+++ scripts/install-snap-ci.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+tar -cvf dist.tar dist/
+
+scp ./dist.tar root@112.124.43.62:/tmp/
+
+ssh root@112.124.43.62 /bin/bash << EOF
+rm -fr /opt/works/shumai_homepage/
+mkdir -p /opt/works/shumai_homepage/
+tar xvf /tmp/dist.tar -C /opt/works/shumai_homepage/
+EOF
