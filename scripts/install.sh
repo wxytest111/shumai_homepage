0a1ba1ee6b2804b9df997b6dbf948c3b4b6dbec0
Xinyu <30540047@qq.com>
2015-01-07 13:46:15 +0800

    change server ip

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,9 +14,9 @@ grunt build --force
 
 tar -cvf dist.tar dist/
 
-scp ./dist.tar root@112.124.43.62:/tmp/
+scp ./dist.tar root@115.29.168.206:/tmp/
 
-ssh root@112.124.43.62 /bin/bash << EOF
+ssh root@115.29.168.206 /bin/bash << EOF
 rm -fr /opt/works/shumai_homepage/
 mkdir -p /opt/works/shumai_homepage/
 tar xvf /tmp/dist.tar -C /opt/works/shumai_homepage/
